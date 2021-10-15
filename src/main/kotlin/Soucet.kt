65a14ad093a523997e487d85589dbce0f4a64191
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2021-10-15 08:56:35 +0200

    Update Soucet.kt

--- src/main/kotlin/Soucet.kt
+++ src/main/kotlin/Soucet.kt
@@ -1,8 +1,8 @@
 fun main () {
-    val input: String = readLine()!!;
-    var output: Int = 0;
+    val input: String = readLine()!!
+    var output: Int = 0
     for (c in input.toCharArray()) {
-        if(c.isDigit()) output += c.digitToInt();
+        if(c.isDigit()) output += c.digitToInt()
     }
     print(output)
 }
